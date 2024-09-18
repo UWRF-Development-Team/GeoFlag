3aa8865763442b8fd722a6842370e289203d5dec
Oliver Lear Sigwarth <sigwartholiver2@gmail.com>
2024-09-18 08:54:00 -0500

    Add Country.java to define the structure of a country with attributes and constructor

---
+++ src/main/java/org/falcon/models/Country.java
@@ -0,0 +1,18 @@
+package org.falcon.models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Country {
+    private static String name;
+    private static Coordinates location;
+    private static String flag;
+    private static String capital;
+    private static String animal;
+    private static int numIslands;
+    private static List<String> landmarks;
+
+    Country(String name, Coordinates location, String flag, String capital, String animal, int numIslands, List<String>landmarks){
+
+    }
+}
