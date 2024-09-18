7b3c23a9d3aa5e2d085f043546c29b847933c316
Oliver Lear Sigwarth <sigwartholiver2@gmail.com>
2024-09-18 08:53:32 -0500

    Add Coordinates.java to represent a geographical coordinate with latitude and longitude

---
+++ src/main/java/org/falcon/models/Coordinates.java
@@ -0,0 +1,10 @@
+package org.falcon.models;
+
+public class Coordinates {
+    private int latitude;
+    private int longitude;
+    Coordinates(int latitude, int longitude) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+}
