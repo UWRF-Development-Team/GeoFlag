6aeef1e14a7da1e67ad9924c383cbbd1b6b0d901
Oliver Lear Sigwarth <sigwartholiver2@gmail.com>
2024-10-10 16:49:17 -0500

    Add city, country, district, province, state, territory, and population fields to Identifier.java

--- src/main/java/org/falcon/models/identifier/Identifier.java
+++ src/main/java/org/falcon/models/identifier/Identifier.java
@@ -1,20 +1,14 @@
 package org.falcon.models.identifier;
+import org.falcon.models.geo.Continent;
+import org.falcon.models.geo.Coord;
+import org.falcon.models.geoIdentifier.*;
+
 import java.util.Optional;
 import java.util.TimeZone;
 
-import org.falcon.models.Continent;
-import org.falcon.models.Coordinates;
-import org.falcon.models.identifier.Animal;
-import org.falcon.models.identifier.Capital;
-import org.falcon.models.identifier.Crest;
-import org.falcon.models.identifier.Flag;
-import org.falcon.models.identifier.Island;
-import org.falcon.models.identifier.Landmarks;
-import org.falcon.models.identifier.Name;
-
 public class Identifier {
     Optional<Continent> continent;
-    Optional<Coordinates> coordinates;
+    Optional<Coord> coordinates;
     Optional<Animal> animal;
     Optional<Capital> capital;
     Optional<Crest> crest;
@@ -23,7 +17,54 @@ public class Identifier {
     Optional<Landmarks> landmarks;
     Optional<Name> name;
     Optional<TimeZone> timeZone;
-    //typically the comment goes here.
+    Optional<City> city;
+    Optional<Country> country;
+    Optional<District> district;
+    Optional<Province> province;
+    Optional<State> state;
+    Optional<Territory> territory;
+    Optional<Population> population;
+    public Identifier(Continent continent, Coordinates coordinates, Animal animal, Capital capital, Crest crest, Flag flag,
+                      Island island, Landmarks landmarks, Name name, TimeZone timezone) {
+        this.continent = Optional.of(continent);
+        this.coordinates = Optional.of(coord);
+        this.animal = Optional.of(animal);
+        this.capital = Optional.of(capital);
+        this.crest = Optional.of(crest);
+        this.flag = Optional.of(flag);
+        this.island = Optional.of(island);
+        this.landmarks = Optional.of(landmarks);
+        this.name = Optional.of(name);
+        this.timeZone = Optional.of(timezone);
+        this.city = Optional.of(city);
+        this.country = Optional.of(country);
+        this.district = Optional.of(district);
+        this.province = Optional.of(province);
+        this.state = Optional.of(state);
+        this.territory = Optional.of(district);
+        this.population = Optional.of(population);
+
+    }
+    public Identifier() {
+        this.continent = Optional.empty();
+        this.coordinates = Optional.empty();
+        this.animal = Optional.empty();
+        this.capital = Optional.empty();
+        this.crest = Optional.empty();
+        this.flag = Optional.empty();
+        this.island = Optional.empty();
+        this.landmarks= Optional.empty();
+        this.name = Optional.empty();
+        this.timeZone = Optional.empty();
+        this.city = Optional.empty();
+        this.country = Optional.empty();
+        this.district = Optional.empty();
+        this.province = Optional.empty();
+        this.state = Optional.empty();
+        this.territory = Optional.empty();
+        this.population = Optional.empty();
+    }
+        //typically the comment goes here.
     public boolean identify(Identifiable itemToIdentify) {
         
         //
@@ -41,3 +82,5 @@ public class Identifier {
 //    }
 
 }
+
+
