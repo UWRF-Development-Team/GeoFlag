451e59c748dab7a6d684718d771ba8e89faffa25
Oliver Lear Sigwarth <sigwartholiver2@gmail.com>
2024-10-10 16:53:37 -0500

    Add State.java to represent geographical states within the geoIdentifier module

---
+++ src/main/java/org/falcon/models/geoIdentifier/State.java
@@ -0,0 +1,16 @@
+package org.falcon.models.geoIdentifier;
+
+import org.falcon.models.geo.CoordRange;
+import org.falcon.models.geo.TimeZones;
+import org.falcon.models.geoIdentifier.attributes.Name;
+
+public class State extends GeoIdentifier{
+    public State() {
+        super();
+    }
+    public State(Name name,
+                 CoordRange coordRange,
+                 TimeZones timeZones) {
+        super(name, coordRange, timeZones);
+    }
+}
