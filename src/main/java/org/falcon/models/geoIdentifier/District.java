4996d317a38aa468da2eaa01235bb334f7ea3f1e
Oliver Lear Sigwarth <sigwartholiver2@gmail.com>
2024-10-10 16:41:46 -0500

    Add District.java to represent a geographic district with name, coordinate range, and time zones

---
+++ src/main/java/org/falcon/models/geoIdentifier/District.java
@@ -0,0 +1,17 @@
+package org.falcon.models.geoIdentifier;
+
+import org.falcon.models.geo.CoordRange;
+import org.falcon.models.geo.TimeZones;
+import org.falcon.models.geoIdentifier.attributes.Name;
+
+public class District extends GeoIdentifier {
+    public District() {
+        super();
+    }
+
+    public District(Name name,
+                    CoordRange coordRange,
+                    TimeZones timeZones) {
+        super(name, coordRange, timeZones);
+    }
+}
