de33868c9a851880cd5f88115e5049423131ec3c
Oliver Lear Sigwarth <sigwartholiver2@gmail.com>
2024-11-13 16:46:27 -0600

    Implement GeoLocatable interface and remove outdated code in GeoIdentifier.java

--- src/main/java/org/falcon/models/geoIdentifier/GeoIdentifier.java
+++ src/main/java/org/falcon/models/geoIdentifier/GeoIdentifier.java
@@ -2,32 +2,9 @@ package org.falcon.models.geoIdentifier;
 
 import org.falcon.models.geo.*;
 import org.falcon.models.geoIdentifier.attributes.Name;
+import org.falcon.models.guess.GeoLocatable;
 
-import java.util.Optional;
-
-public class GeoIdentifier {
-//    private Optional<Country> country;
-//    private Optional<City> city;
-//    private Optional<Capital> capital;
-//    private Optional<TimeZone> timezone;
-//
-//    public GeoIdentifier() {
-//        this.country = Optional.empty();
-//        this.city = Optional.empty();
-//        this.capital = Optional.empty();
-//        this.timezone = Optional.empty();
-//
-//    }
-//    public GeoIdentifier(Country country,
-//                        City city, Capital capital,
-//                        TimeZone timezone) {
-//        this.country = Optional.of(country);
-//        this.city = Optional.of(city);
-//        this.capital = Optional.of(capital);
-//        this.timezone = Optional.of(timezone);
-//
-//
-//    }
+public class GeoIdentifier implements GeoLocatable {
     protected Name name;
     protected CoordRange coordRange;
     protected TimeZones timeZones;
@@ -42,5 +19,17 @@ public class GeoIdentifier {
         this.name = name;
         this.coordRange = coordRange;
         this.timeZones = timeZones;
+    }
+    @Override
+    public boolean equals(Object comparedObject) {
+        if (comparedObject instanceof Name comparedName) {
+            return this.name.equals(comparedName);
+        }
+        return false;
+    }
+
+    @Override
+    public boolean matches(GeoLocatable comparedGeo) {
+        return this.equals(comparedGeo);
     }
 }
