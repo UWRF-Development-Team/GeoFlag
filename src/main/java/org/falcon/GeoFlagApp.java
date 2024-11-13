e15a292636bbc33fb18cf29ca3d453b8b3c1cd31
Oliver Lear Sigwarth <sigwartholiver2@gmail.com>
2024-11-13 17:39:22 -0600

    Add GeoFlagApp.java to initialize the Spring Boot application

---
+++ src/main/java/org/falcon/GeoFlagApp.java
@@ -0,0 +1,13 @@
+package org.falcon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeoFlagApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeoFlagApp.class, args);
+
+    }
+}
