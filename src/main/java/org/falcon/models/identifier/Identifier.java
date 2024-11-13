12a092703b0096db5d30154feeafa08dfd02931a
Oliver Lear Sigwarth <sigwartholiver2@gmail.com>
2024-11-13 17:00:26 -0600

    Remove unnecessary fields and implement Identifiable interface in Identifier.java

--- src/main/java/org/falcon/models/identifier/Identifier.java
+++ src/main/java/org/falcon/models/identifier/Identifier.java
@@ -6,77 +6,12 @@ import org.falcon.models.geoIdentifier.*;
 import java.util.Optional;
 import java.util.TimeZone;
 
-public class Identifier {
-    Optional<Continent> continent;
-    Optional<Coord> coordinates;
-    Optional<Animal> animal;
-    Optional<Capital> capital;
-    Optional<Crest> crest;
-    Optional<Flag> flag;
-    Optional<Island> island;
-    Optional<Landmarks> landmarks;
-    Optional<Name> name;
-    Optional<TimeZone> timeZone;
-    Optional<City> city;
-    Optional<Country> country;
-    Optional<District> district;
-    Optional<Province> province;
-    Optional<State> state;
-    Optional<Territory> territory;
-    Optional<Population> population;
-    public Identifier(Continent continent, Coordinates coordinates, Animal animal, Capital capital, Crest crest, Flag flag,
-                      Island island, Landmarks landmarks, Name name, TimeZone timezone) {
-        this.continent = Optional.of(continent);
-        this.coordinates = Optional.of(coord);
-        this.animal = Optional.of(animal);
-        this.capital = Optional.of(capital);
-        this.crest = Optional.of(crest);
-        this.flag = Optional.of(flag);
-        this.island = Optional.of(island);
-        this.landmarks = Optional.of(landmarks);
-        this.name = Optional.of(name);
-        this.timeZone = Optional.of(timezone);
-        this.city = Optional.of(city);
-        this.country = Optional.of(country);
-        this.district = Optional.of(district);
-        this.province = Optional.of(province);
-        this.state = Optional.of(state);
-        this.territory = Optional.of(district);
-        this.population = Optional.of(population);
+public class Identifier implements Identifiable {
+    //call a database with the type of identifier used
 
-    }
-    public Identifier() {
-        this.continent = Optional.empty();
-        this.coordinates = Optional.empty();
-        this.animal = Optional.empty();
-        this.capital = Optional.empty();
-        this.crest = Optional.empty();
-        this.flag = Optional.empty();
-        this.island = Optional.empty();
-        this.landmarks= Optional.empty();
-        this.name = Optional.empty();
-        this.timeZone = Optional.empty();
-        this.city = Optional.empty();
-        this.country = Optional.empty();
-        this.district = Optional.empty();
-        this.province = Optional.empty();
-        this.state = Optional.empty();
-        this.territory = Optional.empty();
-        this.population = Optional.empty();
-    }
-        //typically the comment goes here.
-    public boolean identify(Identifiable itemToIdentify) {
-        
-        //
-        
+    //compare the name of the identifiable from the database with the name of the identifiable we're trying to match
 
-        return true;
-    }
-
-
-
-
-
+    //if they are equal return true
 //    public boolean hasContinent() {
 //        return this.continent.isPresent();
 //    }
