54729c656d9823a6532530fc26d7fe5ccfde146c
Oliver Lear Sigwarth <sigwartholiver2@gmail.com>
2024-11-13 17:36:46 -0600

    Implement GeoLocatable interface and refactor fields to not use Optional wrapper in Geographical.java

--- src/main/java/org/falcon/models/geo/Geographical.java
+++ src/main/java/org/falcon/models/geo/Geographical.java
@@ -3,11 +3,12 @@ import lombok.Getter;
 import lombok.Setter;
 
 import org.falcon.models.geoIdentifier.attributes.Name;
+import org.falcon.models.guess.GeoLocatable;
 
 import java.util.Optional;
 @Getter
 @Setter
-public class Geographical {
+public class Geographical implements GeoLocatable {
     //private Capital capital;
     //private Coordinates coordinates;
     //private Province province;
@@ -16,31 +17,44 @@ public class Geographical {
     //private Territory territory;
     //private District district;
     //private Continent continent;
-    private Optional<Coord> coord;
-    private Optional<Area> area;
-    private Optional<CoordRange> coordRange;
-    private Optional<Continent> continent;
-    private Optional<Name> name;
-    private Optional<TimeZones> timeZones;
+    private Coord coord;
+    private Area area;
+    private CoordRange coordRange;
+    private Continent continent;
+    private Name name;
+    private TimeZones timeZones;
 
     public Geographical() {
-        this.coord = Optional.empty();
-        this.area = Optional.empty();
-        this.coordRange = Optional.empty();
-        this.continent = Optional.empty();
-        this.name = Optional.empty();
-        this.timeZones = Optional.empty();
+        this.coord = new Coord();
+        this.area = new Area();
+        this.coordRange = new CoordRange();
+        this.continent = new Continent();
+        this.name = new Name();
+        this.timeZones = new TimeZones();
     }
     public Geographical(Coord coord,
                         Area area, CoordRange coordRange,
-                        Continent continent, Name name,TimeZones timezone) {
-        this.coord = Optional.of(coord);
-        this.area = Optional.of(area);
-        this.coordRange = Optional.of(coordRange);
-        this.continent = Optional.of(continent);
-        this.name = Optional.of(name);
-        this.timeZones = Optional.of(timezone);
+                        Continent continent, Name name,
+                        TimeZones timezone) {
+        this.coord = coord;
+        this.area = area;
+        this.coordRange = coordRange;
+        this.continent = continent;
+        this.name = name;
+        this.timeZones = timezone;
 
+    }
+    @Override
+    public boolean equals(Object comparedObject) {
+        if (comparedObject instanceof Name comparedName) {
+            return this.name.equals(comparedName);
+        }
+        return false;
+    }
+
+    @Override
+    public boolean matches(GeoLocatable comparedGeo) {
+        return this.equals(comparedGeo);
     }
 
 
