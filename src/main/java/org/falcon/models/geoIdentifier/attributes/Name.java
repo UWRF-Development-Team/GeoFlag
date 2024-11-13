b970471d25da721a206c13c9bbf37ed57a5b3096
Oliver Lear Sigwarth <sigwartholiver2@gmail.com>
2024-11-13 17:37:38 -0600

    Add constructors and equals method to Name.java to handle nameText initialization and comparison

--- src/main/java/org/falcon/models/geoIdentifier/attributes/Name.java
+++ src/main/java/org/falcon/models/geoIdentifier/attributes/Name.java
@@ -1,4 +1,18 @@
 package org.falcon.models.geoIdentifier.attributes;
 
 public class Name {
+    private String nameText;
+    public Name() {
+        this.nameText = "";
+    }
+    public Name(String nameText) {
+        this.nameText = nameText;
+    }
+    @Override
+    public boolean equals(Object comparedObject) {
+        if (comparedObject instanceof Name comparedName) {
+            return this.nameText.equals(comparedName.nameText);
+        }
+        return false;
+    }
 }
