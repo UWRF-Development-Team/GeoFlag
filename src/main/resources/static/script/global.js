da591fa0f8272168c0c62c4c1e16b93897eb53da
Oliver Lear Sigwarth <sigwartholiver2@gmail.com>
2024-11-26 17:37:48 -0600

    Add dark mode button in global.js to allow for toggling dark mode UI

--- src/main/resources/static/script/global.js
+++ src/main/resources/static/script/global.js
@@ -1,8 +1,20 @@
-import {apiKey} from "./environment";
 
-
+console.log('global.js loaded');
 const gameWindow = document.getElementById('game-window');
-gameWindow.src = setGoogleMapsUrl();
-function setGoogleMapsUrl() {
-    return `https://www.google.com/maps/embed/v1/place?key=${apiKey}&q=Space+Needle,Seattle+WA`;
+// gameWindow.src = setGoogleMapsUrl();
+// console.log(apiKey);
+// function setGoogleMapsUrl() {
+//     return `https://www.google.com/maps/embed/v1/place?key=${apiKey}&q=Space+Needle,Seattle+WA`;
+// }
+const darkModeButton = document.getElementById('dark-mode-button');
+const body = document.getElementsByName('body')
+
+darkModeButton.onclick = function() {
+    if (body.classList.contains('dark-mode')) {
+        body.classList.remove('dark-mode')
+    }
+    else {
+        body.classList.add('dark-mode')
+    }
 }
+
