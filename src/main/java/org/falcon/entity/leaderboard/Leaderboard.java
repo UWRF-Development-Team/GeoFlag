0e0d2353102ccfe707e4089dc7ecb32ee933a7b8
Oliver Lear Sigwarth <sigwartholiver2@gmail.com>
2024-11-26 17:43:46 -0600

    Refactor Leaderboard.java to store multiple users per score and add placement retrieval method

--- src/main/java/org/falcon/entity/leaderboard/Leaderboard.java
+++ src/main/java/org/falcon/entity/leaderboard/Leaderboard.java
@@ -1,8 +1,10 @@
 package org.falcon.entity.leaderboard;
-import java.util.ArrayList;
+import java.util.List;
 import lombok.Getter;
 import lombok.Setter;
+import org.falcon.entity.user.Rank;
 import org.falcon.entity.user.User;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -10,10 +12,42 @@ import java.util.Map;
 @Getter
 @Setter
 public class Leaderboard {
-  // Do we want a leaderboard for every day? That could be the ID?
-  private Map<Integer, User> Leaderboard = new HashMap<>();
-  public void adduser (User user){
-    // Before we insert, we need to check the rank with the other ranks
 
-  }
+    private Map<Integer, List<User>> leaderboard;
+    @Id
+    int placement;
+
+
+    public Leaderboard() {
+        this.leaderboard = new HashMap<>();
+    }
+
+    public Leaderboard(Map<Integer, List<User>> Leaderboard) {
+        this.leaderboard = Leaderboard;
+    }
+
+
+    public void adduser(User user) {
+        // check to see if there are multiple users with the same score
+        List<User> userRanks = this.leaderboard.get(user.getScore());
+        userRanks.add(user);
+        this.leaderboard.put(user.getScore(), userRanks);
+    }
+
+    public Rank getPlacement(User user) {
+        Rank placementValue = new Rank();
+        // Check the score
+        int user_score = user.getScore();
+        // Find placement amongst everyone else
+
+        if (leaderboard.containsKey(user_score)) {
+            leaderboard.get(user_score).forEach((entry) -> {
+                if (entry.getId().equals(user.getId())) {
+                    placementValue = entry.getRank();
+                }});
+        }
+
+        // Return their correct placement based on score
+       return placementValue;
+    }
 }
