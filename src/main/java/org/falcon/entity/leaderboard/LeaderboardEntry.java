159fbcbaa0455eca8a076ab8419c87cee81bb4b3
theoliverlear <sigwartholiver2@gmail.com>
2024-11-27 17:28:49 -0600

    Add unique constraints for ranking and leaderboard fields and fix column mapping relationships in LeaderboardEntry.java

--- src/main/java/org/falcon/entity/leaderboard/LeaderboardEntry.java
+++ src/main/java/org/falcon/entity/leaderboard/LeaderboardEntry.java
@@ -6,20 +6,31 @@ import org.falcon.entity.user.User;
 import java.util.ArrayList;
 import java.util.List;
 
-@Table(name = "leaderboard_entries")
+@Table(
+        name = "leaderboard_entries",
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = {"leaderboard_id", "rank"})
+        }
+)
 @Entity
 public class LeaderboardEntry {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @JoinColumn(name = "leaderboard_entry_id")
+    @Column(name = "leaderboard_entry_id")
     private Long id;
-    @JoinColumn(name = "leaderboard_id")
-    @OneToMany(mappedBy = "leaderboardEntry", cascade = CascadeType.ALL)
-    List<User> users;
+    @Column(name = "rank")
+    private int rank;
+    @ManyToOne
+    @JoinColumn(name = "leaderboard_id", nullable = false)
+    private Leaderboard leaderboard;
+    @ManyToMany(mappedBy = "leaderboardEntries")
+    private List<User> users;
     public LeaderboardEntry() {
+        this.rank = 0;
         this.users = new ArrayList<>();
     }
-    public LeaderboardEntry(List<User> users) {
+    public LeaderboardEntry(int rank, List<User> users) {
+        this.rank = rank;
         this.users = users;
     }
 }
