cef5b9fb52718f5e0f12cfeb63bc22faeda4bf2b
theoliverlear <sigwartholiver2@gmail.com>
2024-11-27 11:54:01 -0600

    Switch LeaderboardRepository.java to extend JpaRepository

--- src/main/java/org/falcon/repository/LeaderboardRepository.java
+++ src/main/java/org/falcon/repository/LeaderboardRepository.java
@@ -3,8 +3,8 @@ package org.falcon.repository;
 import org.falcon.entity.leaderboard.Leaderboard;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface LeaderboardRepository extends CrudRepository<Leaderboard, Long> {
+public interface LeaderboardRepository extends JpaRepository<Leaderboard, Long> {
     Page<Leaderboard> findAll(Pageable pageable);
 }
