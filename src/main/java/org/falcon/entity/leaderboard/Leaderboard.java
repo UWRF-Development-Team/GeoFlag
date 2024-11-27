023aab1608fb02c7b3b251be947b4cc7f80fd057
theoliverlear <sigwartholiver2@gmail.com>
2024-11-27 11:50:58 -0600

    Update Leaderboard entity mappings to sub-entity LeaderboardEntry in Leaderboard.java

--- src/main/java/org/falcon/entity/leaderboard/Leaderboard.java
+++ src/main/java/org/falcon/entity/leaderboard/Leaderboard.java
@@ -6,26 +6,27 @@ import lombok.Setter;
 import org.falcon.entity.user.Rank;
 import org.falcon.entity.user.User;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /** Idk. Kinda confused here ngl */
 @Getter
 @Setter
 @Entity
-@Table
+@Table(name = "leaderboard")
 public class Leaderboard {
+
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
-  private long id;
-  //We can also go another route with this, I'm open to suggestions
-  @Column(unique = true, nullable = false)
-  private String username;
-
-  @Embedded
-  private Rank rank;
-
-  // Merge this with already created functions
-  //private Map<Integer, User> Leaderboard = new HashMap<>();
+  private Long id;
+  @OneToMany(mappedBy = "leaderboard", cascade = CascadeType.ALL)
+  private Map<Integer, LeaderboardEntry> leaderboard;
+  public Leaderboard() {
+    this.leaderboard = new HashMap<>();
+  }
+  public Leaderboard(Map<Integer, LeaderboardEntry> leaderboard) {
+    this.leaderboard = leaderboard;
+  }
   public void adduser (User user){
     // Before we insert, we need to check the rank with the other ranks
 
