1ee83183d31c739f41b2f1d8c565347b0bc4c870
Oliver Lear Sigwarth <sigwartholiver2@gmail.com>
2025-03-30 15:54:47 -0500

    Refactor Coord.java to replace latitude and longitude types with Point model and add constructors for initialization

--- src/main/java/org/falcon/models/geo/Coord.java
+++ src/main/java/org/falcon/models/geo/Coord.java
@@ -5,7 +5,15 @@ import lombok.Setter;
 
 @Getter
 @Setter
-public class Coord extends Geographical {
-    protected double longitude;
-    protected double latitude;
+public class Coord {
+    protected Point longitude;
+    protected Point latitude;
+    public Coord() {
+        this.longitude = new Point();
+        this.latitude = new Point();
+    }
+    public Coord(Point longitude, Point latitude) {
+        this.longitude = longitude;
+        this.latitude = latitude;
+    }
 }
